0e156cf23c507ce0be456ee3777158fae038a0b6
khushi vadhvana <98330791+K-VADHVANA@users.noreply.github.com>
2023-07-27 21:38:16 +0530

    Add files via upload

---
+++ side_nav.js
@@ -0,0 +1,24 @@
+/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+function openNav() {
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+}
+/*function hide(){
+   var x = document.getElementsByClassName("nav_side");
+    if (x.style.display === "none") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "flex";
+    }
+  }*/
+  
+  function sorry() {
+    alert("This part is still under construction.");
+}
+ 
